d3e19323061a7f9c0083b2f1734fe448f341e644
Dustin Stiles <duwstiles@gmail.com>
2022-04-10 13:46:49 -0400

    Initial project setup

---
+++ Application/Migration/1649611200.sql
@@ -0,0 +1,9 @@
+CREATE TABLE comments (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL,
+    post_id UUID NOT NULL,
+    author TEXT NOT NULL,
+    body TEXT NOT NULL,
+    PRIMARY KEY(id, author)
+);
+CREATE INDEX comments_post_id_index ON comments (post_id);
+ALTER TABLE comments ADD CONSTRAINT comments_ref_post_id FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE NO ACTION;
